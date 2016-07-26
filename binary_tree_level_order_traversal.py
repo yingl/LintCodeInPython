6f45df6fa3efec1b91aaacf67e47020f29b39d3e
yingl <linying_43151@163.com>
2016-07-26 14:31:36 +0800

    二叉树的层次遍历

---
+++ binary_tree_level_order_traversal.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: Level order in a list of lists of integers
+    """
+    def levelOrder(self, root):
+        # write your code here
+        if not root:
+            return []
+        ret = []
+        level = [root]
+        while level:
+            ret.append([])
+            new_level = []
+            for node in level:
+                ret[-1].append(node.val)
+                if node.left:
+                    new_level.append(node.left)
+                if node.right:
+                    new_level.append(node.right)
+            level = new_level
+        return ret
