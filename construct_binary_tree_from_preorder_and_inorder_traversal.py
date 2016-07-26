d1e2e5057ee365f3b49f828771c0cb8ca8a90aef
yingl <linying_43151@163.com>
2016-07-26 13:37:22 +0800

    前序遍历和中序遍历树构造二叉树

---
+++ construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param preorder : A list of integers that preorder traversal of a tree
+    @param inorder : A list of integers that inorder traversal of a tree
+    @return : Root of a tree
+    """
+    def buildTree(self, preorder, inorder):
+        # write your code here
+        return self._buildTree(preorder, 0, len(preorder), inorder, 0, len(inorder))
+
+    def _buildTree(self, preorder, pre_start, pre_end, inorder, in_start, in_end):
+        if in_start >= in_end:
+            return None
+        i = in_start
+        while i < in_end:
+            if inorder[i] == preorder[pre_start]:   # 找到根节点
+                break
+            i += 1
+        root = TreeNode(inorder[i])
+        left_len = i - in_start # 左子树元素数量
+        # 生成左子树
+        root.left = self._buildTree(preorder, pre_start + 1, pre_start +1 + left_len, inorder, in_start, i)
+        # 生成右子树
+        root.right = self._buildTree(preorder, pre_start + 1 + left_len, pre_end, inorder, i + 1, in_end)
+        return root
