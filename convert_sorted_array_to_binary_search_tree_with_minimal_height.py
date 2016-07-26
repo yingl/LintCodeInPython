af0e2ac814e7c1b37deb0779e92acafc59ae90cc
yingl <linying_43151@163.com>
2016-07-26 14:28:02 +0800

    把排序数组转换为高度最小的二叉搜索树

---
+++ convert_sorted_array_to_binary_search_tree_with_minimal_height.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param A: a list of integer
+    @return: a tree node
+    """
+    def sortedArrayToBST(self, A):
+        # write your code here
+       if not A:
+            return None
+        return self._sortedArrayToBST(A, 0, len(A))
+
+    def _sortedArrayToBST(self, array, start, end):
+        if start >= end:
+            return None
+        mid = (start + end) / 2
+        root = TreeNode(array[mid])
+        root.left = self._sortedArrayToBST(array, start, mid)   # 生成左子树
+        root.right = self._sortedArrayToBST(array, mid + 1, end)    # 生成右子树
+        return root
