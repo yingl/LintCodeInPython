f642163f28e5677cf3b9cd77062eabf66b18685f
yingl <linying_43151@163.com>
2016-07-27 17:27:32 +0800

    判断字符串是否没有重复字符

---
+++ unique_characters.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param s: a string
+    # @return: a boolean
+    def isUnique(self, str):
+        # write your code here
+        char_map = {}
+        for ch in str:
+            if ch not in char_map:
+                char_map[ch] = True
+            else:
+                return False
+        return True
