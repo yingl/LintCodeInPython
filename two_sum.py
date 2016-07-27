ece3f2d03141a138254ba960ccbbe8e816f0a985
yingl <linying_43151@163.com>
2016-07-27 17:38:55 +0800

    两数之和

---
+++ two_sum.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param numbers : An array of Integer
+    @param target : target = numbers[index1] + numbers[index2]
+    @return : [index1 + 1, index2 + 1] (index1 < index2)
+    """
+    def twoSum(self, numbers, target):
+        # write your code here
+        for i in xrange(numbers - 1):
+            for j in xrange(i + 1, numbers):
+                if (numbers[i] + numbers[j]) == target:
+                    return [i + 1, j + 1]
+        return [0, 0]
