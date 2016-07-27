cb5cdf2aa0017f25d917437355a99ff10d3448c2
yingl <linying_43151@163.com>
2016-07-27 17:41:57 +0800

    两个字符串是变位词

---
+++ two_strings_are_anagrams.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param s: The first string
+    @param b: The second string
+    @return true or false
+    """
+    def anagram(self, s, t):
+        # write your code here
+        char_map = {}
+        for ch in s:
+            if ch not in char_map:
+                char_map[ch] = 1
+            else:
+                char_map[ch] += 1
+        for ch in t:
+            if ch not in char_map:
+                return False
+            else:
+                char_map[ch] -= 1
+        for key, value in char_map.items():
+            if value != 0:
+                return False
+        return True
