802f3e33916b9d637e0683d6dad1ff107f2e881f
yingl <linying_43151@163.com>
2016-07-27 21:12:00 +0800

    尾部的零

---
+++ trailing_zeros.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param n a integer
+    # @return ans a integer
+    def trailingZeros(self, n):
+        zeros = 0
+        while n > 0:
+            zeros += n / 5
+            n /= 5
+        return zeros
