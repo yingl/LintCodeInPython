e7ebf02f1dabd8ad96fb17fc8c393624465a7fc7
yingl <linying_43151@163.com>
2016-07-27 14:08:58 +0800

    有效的括号序列

---
+++ valid_parentheses.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param {string} s A string
+    # @return {boolean} whether the string is a valid parentheses
+    def isValidParentheses(self, s):
+        # Write your code here
+        par_list = []
+        for ch in s:
+            if (ch == '(') or (ch == '[') or (ch == '{'):
+                par_list.append(ch) # 插入左括号
+            else:
+                if not self.check(ch, par_list):
+                    return False
+        return True if not par_list else False
+
+    def check(self, right, par_list):
+        if not par_list:
+            return False
+        left = par_list[-1]
+        if ((left == '(') and (right == ')')) or \
+                ((left == '[') and (right == ']')) or \
+                ((left == '{') and (right == '}')):
+            par_list.pop()  # 括号匹配成功后弹出
+        else:
+            par_list.append(right)
+        return True
