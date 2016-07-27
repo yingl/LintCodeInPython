af0b90c0d48d67b877918c625440c946e8151890
yingl <linying_43151@163.com>
2016-07-27 10:28:52 +0800

    字符串查找

---
+++ strstr.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    def strStr(self, source, target):
+        if (source is None) or (target is None):
+            return -1
+        for i in xrange(len(source) - len(target) + 1):
+            if source[i:i + len(target)] == target:
+                return i
+        return -1
