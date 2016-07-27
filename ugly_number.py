fc9473b54c5dfdf8c54cb579623abe72ab7d7d95
yingl <linying_43151@163.com>
2016-07-27 17:29:04 +0800

    丑数

---
+++ ugly_number.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param {int} num an integer
+    # @return {boolean} true if num is an ugly number or false
+    def isUgly(self, num):
+        # Write your code here
+        if (num <= 0):
+            return False
+        if (num == 1) or (num == 2) or \
+                (num == 3) or (num == 5):
+            return True
+        else:
+            if (num % 5 == 0):
+                return self.isUgly(num / 5)
+            elif (num % 3 == 0):
+                return self.isUgly(num / 3)
+            elif (num % 2 == 0):
+                return self.isUgly(num / 2)
+            else:
+                return False
