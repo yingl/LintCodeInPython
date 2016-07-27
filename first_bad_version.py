8f1da1e83b46cf088557d60ef48d724ab5b18aec
yingl <linying_43151@163.com>
2016-07-27 10:23:30 +0800

    第一个错误的代码版本

---
+++ first_bad_version.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param n: An integers.
+    @return: An integer which is the first bad version.
+    """
+    def findFirstBadVersion(self, n):
+        # write your code here
+        if n <= 0:
+            return 0
+        start, end = 1, n + 1
+        while start < end:
+            mid = (start + end) / 2
+            if SVNRepo.isBadVersion(mid):
+                end = mid   # 错误范围[start, mid]
+            else:
+                start = mid + 1 # 错误在后半部分
+        return start
