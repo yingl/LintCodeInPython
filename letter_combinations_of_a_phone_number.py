86859786aed3b62381692dffc2a6b03146737309
yingl <linying_43151@163.com>
2016-07-27 13:29:20 +0800

    电话号码的字母组合

---
+++ letter_combinations_of_a_phone_number.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param {string} digits A digital string
+    # @return {string[]} all posible letter combinations
+    def letterCombinations(self, digits):
+        # Write your code here
+        if not digits:
+            return []
+        key_map = ['abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+        level = ['']
+        i = 0
+        while i < len(digits):
+            pos = int(digits[i]) - 2
+            if pos >= 0:
+                new_level = []
+                for comb in level:
+                    for ch in key_map[pos]:
+                        new_level.append(comb + ch)
+                level = new_level
+            i += 1
+        return level
