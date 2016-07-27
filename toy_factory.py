f15f1dff2733faa8294dc41635d4dc334e8fd022
yingl <linying_43151@163.com>
2016-07-27 21:13:07 +0800

    玩具工厂

---
+++ toy_factory.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+class Toy:
+    def talk(self):
+        raise NotImplementedError('This method should have implemented.')
+
+class Dog(Toy):
+    # Write your code here
+    def talk(self):
+        print 'Wow'
+
+class Cat(Toy):
+    # Write your code here
+    def talk(self):
+        print 'Meow'
+
+class ToyFactory:
+    # @param {string} shapeType a string
+    # @return {Toy} Get object of the type
+    def getToy(self, type):
+        # Write your code here
+        if type == 'Dog':
+            return Dog()
+        elif type == 'Cat':
+            return Cat()
