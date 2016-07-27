22f4a47ed0fd96e60ce084c282f24e72743334bc
yingl <linying_43151@163.com>
2016-07-27 13:40:08 +0800

    验证二叉查找树

---
+++ validate_binary_search_tree.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: True if the binary tree is BST, or false
+    """  
+    def isValidBST(self, root):
+        # write your code here
+        if root is None:
+            return True
+        return self.check(root.left, root.val, None) and \
+                self.check(root.right, None, root.val)
+        
+    def check(self, root, left_max, right_min):
+        if root is None:
+            return True
+        if left_max and (root.val >= left_max):
+            return False
+        if right_min and (root.val <= right_min):
+            return False
+        return self.check(root.left, root.val, right_min) and \
+                self.check(root.right, left_max, root.val)
