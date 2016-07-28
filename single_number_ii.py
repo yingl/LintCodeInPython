6a17911bdb4c9e05f4e3d2d715027ed09eadd5ab
yingl <linying_43151@163.com>
2016-07-29 03:38:02 +0800

    落单的数 II

---
+++ single_number_ii.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+  @param A : An integer array
+  @return : An integer
+    """
+    def singleNumberII(self, A):
+        # write your code here
+        ret = 0
+        tmp = 0
+        for num in A:
+            ret = (ret ^ num) & (~tmp)
+            tmp = (tmp ^ num) & (~ret)
+        return ret
