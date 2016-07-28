5e5f4a4de193de3c38f07ca6a56b86b87b88e38d
yingl <linying_43151@163.com>
2016-07-28 13:03:01 +0800

    用递归打印数字

---
+++ print_numbers_by_recursion.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param n: An integer.
+    # return : A list of integer storing 1 to the largest number with n digits.
+    def numbersByRecursion(self, n):
+        # write your code here
+        self.ret = []
+        if n > 0:
+            self._numbersByRecursion(1, n)
+        return self.ret
+
+    def _numbersByRecursion(self, val, n):
+        new_val = val * 10  # 把递归降低到n次
+        for i in xrange(val, new_val):
+            self.ret.append(i)
+        if n == 1:
+            return
+        self._numbersByRecursion(new_val, n - 1)
