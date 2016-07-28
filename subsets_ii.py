5b299c4ff7c0f32439f84e1f083a23f9f925f46b
yingl <linying_43151@163.com>
2016-07-28 14:57:17 +0800

    带重复元素的子集

---
+++ subsets_ii.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param S: A set of numbers.
+    @return: A list of lists. All valid subsets.
+    """
+    def subsetsWithDup(self, S):
+        # write your code here
+        ret = []
+        S.sort()
+        for i in xrange(len(S) + 1):
+            combs = self.subset(S, 0, i) # 计算包含i个元素的子集
+            for comb in combs:
+                ret.append(comb)
+        return ret
+        
+    def subset(self, numbers, pos, k):
+        n = len(numbers) - pos
+        if (not numbers) or (n < k):
+            return None
+        ret = []
+        if k == 0:  # 0/1特殊情况处理
+            return [[]]
+        for i in xrange(pos, len(numbers), 1):
+            if (i > pos) and (numbers[i] == numbers[i - 1]):
+                continue
+            combs = self.subset(numbers, i + 1, k - 1)  # 计算i + 1开始位置，包含k - 1个元素的子集
+            if combs:
+                if combs != [[]]:
+                    for comb in combs:
+                        new_comb = [numbers[i]]
+                        new_comb.extend(comb)
+                        ret.append(new_comb)
+                else:
+                    ret.append([numbers[i]])
+        return ret
