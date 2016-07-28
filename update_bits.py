f2ef31d4e5d16fbc1050183ba944199dd94f3adc
yingl <linying_43151@163.com>
2016-07-29 02:53:54 +0800

    更新二进制位

---
+++ update_bits.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    #@param n, m: Two integer
+    #@param i, j: Two bit positions
+    #return: An integer
+    def updateBits(self, n, m, i, j):
+        # write your code here
+        bits = []
+        for k in xrange(32):  # 将n转为二进制bits
+            bits.append(n % 2)
+            n /= 2
+        for k in xrange(i, j + 1):  # 将第i到j位转为m的二进制bits
+            bits[k] = m % 2
+            m /= 2
+        n = 0
+        for k in xrange(31):
+            if bits[k] == 1:
+                n |= (1 << k)
+        if bits[31] == 1:  # 负号
+            n -= (1 << 31)
+        return n
