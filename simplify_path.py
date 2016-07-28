d52ccfd250b77bce23951dcc7e7fae98d393a7eb
yingl <linying_43151@163.com>
2016-07-29 03:41:02 +0800

    简化路径

---
+++ simplify_path.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param {string} path the original path
+    # @return {string} the simplified path
+    def simplifyPath(self, path):
+        # Write your code here
+        dirs = path.split('/')
+        new_path = []
+        for _dir in dirs:
+            if (_dir == '..') and (len(new_path) > 0):
+                new_path.pop(-1)
+            elif _dir and (_dir != '.') and (_dir != '..'):
+                new_path.append(_dir)
+        return '/' + '/'.join(new_path)
