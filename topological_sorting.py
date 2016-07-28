08dcff4f32de301df5faad3fd166ddf4fc9de504
yingl <linying_43151@163.com>
2016-07-29 02:13:42 +0800

    拓扑排序

---
+++ topological_sorting.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param graph: A list of Directed graph node
+    @return: A list of graph nodes in topological order.
+    """
+    def topSort(self, graph):
+        # write your code here
+        ret = []
+        node_map = {}
+        stack = []
+        if graph:
+            for node in graph:  # 更新指向节点的入度
+                for neighbor in node.neighbors:
+                    if neighbor not in node_map:
+                        node_map[neighbor] = 1
+                    else:
+                        node_map[neighbor] += 1
+            for node in graph:  # 找出入度为0的节点
+                if node not in node_map:
+                    stack.append(node)
+            while stack:
+                node = stack.pop()
+                ret.append(node)
+                for neighbor in node.neighbors: # 更新指向节点的入度
+                    node_map[neighbor] -= 1
+                    if node_map[neighbor] == 0:
+                        stack.append(neighbor)
+        return ret
