4deab02796820cfbb12d5e6ca6aff521f8ace4f4
yingl <linying_43151@163.com>
2016-07-29 03:39:40 +0800

    落单的数

---
+++ single_number.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param A : an integer array
+    @return : a integer
+    """
+    def singleNumber(self, A):
+        # write your code here
+        ret = 0
+        for num in A:
+            ret ^= num
+        return ret
