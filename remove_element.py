79a087c4021c847ecf22faaf04858ee4898ea21f
yingl <linying_43151@163.com>
2016-07-30 01:08:16 +0800

    删除元素

---
+++ remove_element.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param A: A list of integers
+    @param elem: An integer
+    @return: The new length after remove
+    """
+    def removeElement(self, A, elem):
+        # write your code here
+        step = 0
+        for i in range(0, len(A)):
+            if A[i] == elem:
+                step += 1
+            else:
+                A[i - step] = A[i]
+        return len(A) - step
