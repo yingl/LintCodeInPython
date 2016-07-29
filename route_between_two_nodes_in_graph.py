a17342d3942da6736d3337c85d2c8180c0381d6e
yingl <linying_43151@163.com>
2016-07-29 13:15:26 +0800

    图中两个点之间的路线

---
+++ route_between_two_nodes_in_graph.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param graph: A list of Directed graph node
+    @param s: the starting Directed graph node
+    @param t: the terminal Directed graph node
+    @return: a boolean value
+    """
+    def hasRoute(self, graph, s, t):
+        # write your code here
+        # 广度优先遍历
+        nodes = [s]
+        node_map = {s: True}
+        pos = 0
+        while pos < len(nodes):
+            if nodes[pos] == t:
+                return True
+            for node in nodes[pos].neighbors:
+                if node not in node_map:
+                    node_map[node] = True
+                    nodes.append(node)
+            pos += 1
+        return False
