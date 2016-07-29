40c4984488ca089644c486c15b091e265b3709b9
yingl <linying_43151@163.com>
2016-07-30 01:27:12 +0800

    爬楼梯

---
+++ climbing_stairs.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param n: An integer
+    @return: An integer
+    """
+    def climbStairs(self, n):
+        # write your code here
+        if (n == 0) or (n == 1):
+            return 1
+        if n == 2:
+            return 2
+        f_1 = 1
+        f_2 = 2
+        for i in xrange(3, n + 1):
+            ret = f_1 + f_2
+            f_1 = f_2
+            f_2 = ret
+        return ret
