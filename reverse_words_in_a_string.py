b05eb59b664b758daf8b8bd954196b3ada1e8a3a
yingl <linying_43151@163.com>
2016-07-29 19:24:48 +0800

    翻转字符串

---
+++ reverse_words_in_a_string.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param s : A string
+    # @return : A string
+    def reverseWords(self, s):
+        # write your code here
+        # 最偷懒的做法，不解释。。。
+        words = s.split()
+        words.reverse()
+        return " ".join(words)
