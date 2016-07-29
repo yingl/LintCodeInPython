66830145bbf38ba76c5b93d67e580a8bc179375d
yingl <linying_43151@163.com>
2016-07-30 01:24:54 +0800

    删除排序链表中的重复元素

---
+++ remove_duplicates_from_sorted_list.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param head: A ListNode
+    @return: A ListNode
+    """
+    def deleteDuplicates(self, head):
+        # write your code here
+        node = head
+        while node is not None:
+            val = node.val
+            next_node = node.next
+            while next_node:
+                if next_node.val == val:
+                    next_node = next_node.next
+                else:
+                    break
+            if next_node != node.next:
+                node.next = next_node
+            node = node.next
+        return head
