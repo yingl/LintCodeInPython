2c7fcca5e43025b3682c10c5f44ae88cfa1c3cda
yingl <linying_43151@163.com>
2016-07-30 00:28:38 +0800

    字符大小写排序

---
+++ sort_letters_by_case.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param chars: The letters array you should sort.
+    """
+    def sortLetters(self, chars):
+        # write your code here
+        i, j = 0, len(chars) - 1
+        while i < j:
+            while (i < len(chars)) and ((chars[i] < 'A') or (chars[i] > 'Z')):
+                i += 1
+            while (j >= 0) and ((chars[j] < 'a') or (chars[j] > 'z')):
+                j -= 1
+            if (i < j):
+                chars[i], chars[j] = chars[j], chars[i]
+                i += 1
+                j -= 1
