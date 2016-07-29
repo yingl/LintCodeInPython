cddc225ce73c6c3d804757f36bc60f7bcbfb7208
yingl <linying_43151@163.com>
2016-07-29 19:38:08 +0800

    删除链表中倒数第n个节点

---
+++ remove_nth_node_from_end_of_list.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param head: The first node of linked list.
+    @param n: An integer.
+    @return: The head of linked list.
+    """
+    def removeNthFromEnd(self, head, n):
+        # write your code here
+        '''
+        第一个node向向前走n步，然后两个node一起走。
+        走的时候记住记住后面一个node的prev节点.
+        '''
+        first, second = head, head
+        for i in xrange(0, n):
+            first = first.next
+        prev = None
+        while first:
+            first = first.next
+            prev = second
+            second = second.next
+        if not prev:  # 删除head节点
+            return head.next
+        else:
+            prev.next = second.next
+            return head
