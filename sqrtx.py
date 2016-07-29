ed70c326c95bca34f3d3f9cdc62666268e29eef8
yingl <linying_43151@163.com>
2016-07-30 00:02:53 +0800

    x的平方根

---
+++ sqrtx.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param x: An integer
+    @return: The sqrt of x
+    """
+    def sqrt(self, x):
+        # write your code here
+        begin, end = 1, x
+        sqrt = (begin + end) / 2
+        while True:
+            square = sqrt * sqrt
+            if square > x:
+                end = sqrt
+                sqrt = int((begin + end) / 2)
+            elif square == x:
+                return sqrt
+            else:
+                begin = sqrt
+                sqrt = int((begin + end) / 2)
+                if sqrt == begin:
+                    return sqrt
