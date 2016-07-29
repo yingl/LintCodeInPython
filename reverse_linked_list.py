f034f137b313f17bc615cfffc08c9c524e2bd98a
yingl <linying_43151@163.com>
2016-07-29 19:26:33 +0800

    翻转链表

---
+++ reverse_linked_list.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param head: The first node of the linked list.
+    @return: You should return the head of the reversed linked list.
+                  Reverse it in-place.
+    """
+    def reverse(self, head):
+        # write your code here
+        if not head:
+            return None
+        new_head = None
+        while head:
+            node = head
+            head = head.next
+            node.next = new_head
+            new_head = node
+        return new_head
