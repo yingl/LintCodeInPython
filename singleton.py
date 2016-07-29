49d5a7d76ac62fa14f07674b6dab47c4135e1718
yingl <linying_43151@163.com>
2016-07-30 00:42:40 +0800

    单例

---
+++ singleton.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @return: The same instance of this class every time
+    @classmethod
+    def getInstance(cls):
+        # write your code here
+        if not Solution.obj:
+            Solution.obj = cls()
+        return Solution.obj
+
+    obj = None
