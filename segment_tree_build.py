424082f51046e1f7a6719c135150904e1455d6e9
yingl <linying_43151@163.com>
2016-07-29 23:43:34 +0800

    线段树的构造

---
+++ segment_tree_build.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+class Solution: 
+    # @param start, end: Denote an segment / interval
+    # @return: The root of Segment Tree
+    def build(self, start, end):
+        # write your code here
+        if start > end:
+            return None
+        ret = SegmentTreeNode(start, end)
+        if start < end:
+            ret.left = self.build(start, (start + end) / 2)
+            ret.right = self.build((start + end) / 2 + 1, end)
+        return ret
