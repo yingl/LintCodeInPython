7506d8fbed394f942d2221c875f86ee168f7c4a3
yingl <linying_43151@163.com>
2016-07-30 01:06:49 +0800

    删除链表中的元素

---
+++ remove_linked_list_elements.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param head, a ListNode
+    # @param val, an integer
+    # @return a ListNode
+    def removeElements(self, head, val):
+        # Write your code here
+        prev, node = None, head
+        while node:
+            if node.val == val:
+                if not prev:
+                    head = node.next
+                else:
+                    prev.next = node.next
+            else:
+                prev = node
+            node = node.next
+        return head
