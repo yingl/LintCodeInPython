a1b4f8da2f5c491a7078bb797a1f28060470578d
yingl <linying_43151@163.com>
2016-07-30 01:18:03 +0800

    删除排序链表中的重复数字 II

---
+++ remove_duplicates_from_sorted_list_ii.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param head: A ListNode
+    @return: A ListNode
+    """
+    def deleteDuplicates(self, head):
+        # write your code here
+        new_head, new_tail = None, None
+        old_node = head
+        while old_node:
+            next_node = old_node.next
+            if (next_node is None) or (next_node.val != old_node.val):
+                if new_head is None:
+                    new_head = old_node
+                else:
+                    new_tail.next = old_node
+                new_tail = old_node
+                new_tail.next = None
+                old_node = next_node
+            else:
+                # 跳过重复的节点
+                while next_node and (next_node.val == old_node.val):
+                    next_node = next_node.next
+                old_node = next_node
+        return new_head
