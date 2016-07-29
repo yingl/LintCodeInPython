387012d8dc1b4268c3adfe42e6bc19119c045ded
yingl <linying_43151@163.com>
2016-07-29 14:09:28 +0800

    旋转链表

---
+++ rotate_list.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param head: the list
+    # @param k: rotate to the right k places
+    # @return: the list after rotation
+    def rotateRight(self, head, k):
+        # write your code here
+        if not head:
+            return head
+        list_len = 0
+        tail = None
+        node = head
+        while node: # 找到链表的尾部节点并统计链表长度
+            list_len += 1
+            tail = node
+            node = node.next
+        shift = k % list_len
+        if shift == 0:
+            return head
+        new_tail = head
+        for i in xrange(list_len - shift-1):
+            new_tail = new_tail.next
+        new_head = new_tail.next  # 找到新的head
+        tail.next = head  # tail指向原来的head
+        new_tail.next = None
+        return new_head
