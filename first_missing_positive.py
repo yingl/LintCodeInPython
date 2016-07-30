d9724a9cf325a4f08d0e97cb1a4e2ab18557bab8
yingl <linying_43151@163.com>
2016-07-30 11:56:19 +0800

    丢失的第一个正整数

---
+++ first_missing_positive.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param A, a list of integers
+    # @return an integer
+    def firstMissingPositive(self, A):
+        # write your code here
+        '''
+        对于每个大于等于0的数放到对应的位置上，
+        返回第一个A[i] != i + 1的数。
+        '''
+        i = 0
+        while i < len(A):
+            if (A[i] >= len(A)) or (A[i] < 0):
+                i += 1  # 不符合条件的数不管！
+            elif A[i] != i + 1: # 对于不在对应位置的数要找到正确的位置
+                j = A[i]
+                if A[i] != A[j - 1]:
+                    A[i], A[j - 1] = A[j - 1], A[i]
+                else:
+                    i += 1
+            else:
+                i += 1
+        for i in xrange(0, len(A)):
+            if A[i] != (i + 1):
+                return i + 1
+        return len(A) + 1
