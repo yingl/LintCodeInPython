9c5ef0622d9b7298304a219e129af8c5f51258b8
yingl <linying_43151@163.com>
2016-07-30 11:50:53 +0800

    数组剔除元素后的乘积

---
+++ product_of_array_exclude_itself.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param A: Given an integers array A
+    @return: An integer array B and B[i]= A[0] * ... * A[i-1] * A[i+1] * ... * A[n-1]
+    """
+    def productExcludeItself(self, A):
+        # write your code here
+        B = [1] * len(A)
+        for i in xrange(0, len(A)):
+            for j in xrange(0, len(A)):
+                if j != i:
+                    B[i] *= A[j]
+        return B
