6e731bf21c343676338fed6dab2673755d21cc10
yingl <linying_43151@163.com>
2016-07-30 11:48:09 +0800

    恢复旋转排序数组

---
+++ recover_rotated_sorted_array.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param nums: The rotated sorted array
+    @return: nothing
+    """
+    def recoverRotatedSortedArray(self, nums):
+        # write your code here
+        if nums[0] < nums[-1]:
+            return
+        shift = 0
+        for i in xrange(0, len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                shift = i + 1
+                break
+        self.reverse(nums, 0, shift - 1)
+        self.reverse(nums, shift, len(nums) - 1)
+        self.reverse(nums, 0, len(nums) - 1)
+
+    def reverse(self, nums, start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
