a6fa3e9b6f1a12dd810fe445ce37cd7b2e3df66e
yingl <linying_43151@163.com>
2016-07-31 14:52:04 +0800

    排列序号

---
+++ permutation_index.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param {int[]} A an integer array
+    # @return {long} a long integer
+    def permutationIndex(self, A):
+        # Write your code here
+        ret = 1
+        for i in xrange(0, len(A)):
+            fact, rc = 1, 0  # 初始化阶乘与逆序数
+            for j in xrange(i + 1, len(A)):
+                if A[i] > A[j]:
+                    rc += 1
+                fact *= (j - i)
+            ret += rc * fact
+        return ret
