5a4179a8f2da60fc8ae8e844f2936b888f083f02
yingl <linying_43151@163.com>
2016-07-31 18:24:21 +0800

    带环链表

---
+++ linked_list_cycle.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param head: The first node of the linked list.
+    @return: True if it has a cycle, or false
+    """
+    def hasCycle(self, head):
+        # write your code here
+        one_node, two_node = head, head
+        while two_node:
+            for i in xrange(2):
+                if two_node:
+                    two_node = two_node.next
+                    if two_node == one_node:
+                        return True
+                else:
+                    break
+            one_node = one_node.next
+            if not two_node:
+                break
+        return False
