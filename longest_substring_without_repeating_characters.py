c6461620036ccf829924fa0f49f8d689b909260f
yingl <linying_43151@163.com>
2016-08-01 17:59:23 +0800

    最长无重复字符的子串

---
+++ longest_substring_without_repeating_characters.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param s: a string
+    # @return: an integer
+    def lengthOfLongestSubstring(self, s):
+        # write your code here
+        ret = 0
+        for i in xrange(len(s)):
+            char_map = {} # 记录某个字母是否出现
+            len_of_substr = 0
+            for j in xrange(i, len(s)):
+                if s[j] not in char_map:
+                    char_map[s[j]] = True
+                    len_of_substr += 1
+                else:
+                    break
+            if len_of_substr > ret:
+                ret = len_of_substr
+        return ret
