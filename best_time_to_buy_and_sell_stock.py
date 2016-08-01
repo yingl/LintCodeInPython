250dc826dbccb835ba61338bf1b5ca4448b709b7
yingl <linying_43151@163.com>
2016-08-01 08:41:36 +0800

    买卖股票的最佳时机

---
+++ best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param prices: Given an integer array
+    @return: Maximum profit
+    """
+    def maxProfit(self, prices):
+        # write your code here
+        ret = 0
+        if prices:
+            buy_price = prices[0] # 必须先买后卖，假设先在第一天买进。
+            for i in xrange(1, len(prices)):
+                ret = max(ret, prices[i] - buy_price)
+                buy_price = min(prices[i], buy_price) # 更新买入价格
+        return ret
