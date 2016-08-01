168ff46c9838fb4fe06b24fbe52aec821e5975fb
yingl <linying_43151@163.com>
2016-08-01 14:52:50 +0800

    格雷编码

---
+++ gray_code.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param {int} n a number
+    # @return {int[]} Gray code
+    def grayCode(self, n):
+        # Write your code here
+        ret = []
+        for i in xrange(1 << n):
+            ret.append((i >> 1) ^ i)
+        return ret
