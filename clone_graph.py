7ab0e4232393c1d4845bcc25372b78ff92042357
yingl <linying_43151@163.com>
2016-08-01 08:56:37 +0800

    克隆图

---
+++ clone_graph.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param node, a undirected graph node
+    # @return a undirected graph node
+    def __init__(self):
+        self.dict = {}  # 存放已复制的节点
+        
+    def cloneGraph(self, node):
+        if not node:
+            return None
+        # write your code here
+        if node in self.dict:
+            return self.dict[node]  # 该节点已复制
+        ret = UndirectedGraphNode(node.label)
+        self.dict[node] = ret
+        for neighbor in node.neighbors:
+            ret.neighbors.append(self.cloneGraph(neighbor))
+        return ret
