13687e4964201084b8fffac9f94ff5fa362feb7c
yingl <linying_43151@163.com>
2016-08-01 08:33:23 +0800

    更新注释信息

--- sort_colors_ii.py
+++ sort_colors_ii.py
@@ -9,4 +9,6 @@ class Solution:
     def sortColors2(self, colors, k):
         # write your code here
         # 偷个懒，直接用sort。把sort_integers_ii.py搬过来就OK了。
+        # 已知条件包括：包括k种不同的颜色，并按照1到k进行编号。
+        # 所以pivot的选取就比较简单了，从1开始往上加。
         colors.sort()
