5e24baf27b21933d8c0f74aef061edc723ff3065
yingl <linying_43151@163.com>
2016-08-01 08:46:12 +0800

    快速幂

---
+++ fast_power.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param a, b, n: 32bit integers
+    @return: An integer
+    """
+    def fastPower(self, a, b, n):
+        # write your code here
+        if n == 0:
+            return 1 % b
+        if 1 == n:
+            return a % b
+        x = self.fastPower(a, b, n / 2)
+        if n % 2 == 1:
+            return (((x * x) % b) * a) % b
+        else:
+            return (x * x) % b
