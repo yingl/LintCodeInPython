a797748d200736d9ff5140dadedd8b8e812cf877
yingl <linying_43151@163.com>
2016-08-04 09:04:17 +0800

    斐波纳契数列

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param n: an integer
+    # @return an integer f(n)
+    def fibonacci(self, n):
+        # write your code here
+        if (n == 1) or (n == 2):
+            return n - 1
+        ret = 0
+        f_1, f_2 = 0, 1
+        for i in xrange(3, n + 1):
+            ret = f_1 + f_2
+            f_1 = f_2
+            f_2 = ret
+        return ret
