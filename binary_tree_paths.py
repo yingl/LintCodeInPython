d51850b870f6022234f9669c05526bf23e85ff89
yingl <linying_43151@163.com>
2016-08-04 14:20:04 +0800

    二叉树的所有路径

---
+++ binary_tree_paths.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param {TreeNode} root the root of the binary tree
+    # @return {List[str]} all root-to-leaf paths
+    def binaryTreePaths(self, root):
+        # Write your code here
+        self.ret = []
+        self._binaryTreePaths(root, '')
+        return self.ret
+
+    def _binaryTreePaths(self, root, path):
+        if root:
+            path += str(root.val) if not path else ('->' + str(root.val))
+            # print path
+            if root.left:
+                self._binaryTreePaths(root.left, path)
+            if root.right:
+                self._binaryTreePaths(root.right, path)
+            if not (root.left or root.right):
+                self.ret.append(path)
