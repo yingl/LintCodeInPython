58bb990b408f3ef761e28cbd5ee26229380ed451
yingl <linying_43151@163.com>
2016-08-04 13:40:14 +0800

    最长单词

---
+++ longest_words.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param dictionary: a list of strings
+    # @return: a list of strings
+    def longestWords(self, dictionary):
+        # write your code here
+        ret = []
+        max_word_len = 0
+        for word in dictionary:
+            word_len = len(word)
+            if word_len > max_word_len:
+                max_word_len = word_len
+                ret = []
+                ret.append(word)
+            elif word_len == max_word_len:
+                ret.append(word)
+        return ret
