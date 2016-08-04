8db25f0031970f06d1af521b0a573433e8dbd52d
yingl <linying_43151@163.com>
2016-08-04 13:49:02 +0800

    翻转二叉树

---
+++ invert_binary_tree.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param root: a TreeNode, the root of the binary tree
+    # @return: nothing
+    def invertBinaryTree(self, root):
+        # write your code here
+        if root is None:
+            return
+        root.left, root.right = root.right, root.left
+        self.invertBinaryTree(root.left)
+        self.invertBinaryTree(root.right)
