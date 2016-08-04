ff1f53263a1e09fad44e91e72d46b7143020c693
yingl <linying_43151@163.com>
2016-08-04 14:30:34 +0800

    A + B 问题

---
+++ a_b_problem.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param a: The first integer
+    @param b: The second integer
+    @return:  The sum of a and b
+    """
+    def aplusb(self, a, b):
+        # write your code here, try to do it without arithmetic operators.
+        import ctypes
+        a = ctypes.c_int32(a).value
+        b = ctypes.c_int32(b).value
+        while b != 0:
+            carry = ctypes.c_int32(a & b).value
+            a = ctypes.c_int32(a ^ b).value
+            b = ctypes.c_int32(carry << 1).value
+        return a
