ddc89582c1495c80da477fa00cfe573bde1b2d18
yingl <linying_43151@163.com>
2016-08-04 14:05:20 +0800

    将整数A转换为B

---
+++ flip_bits.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param a, b: Two integer
+    return: An integer
+    """
+    def bitSwapRequired(self, a, b):
+        # write your code here
+        a ^= b
+        if a > 0:
+            return self.ones(a)
+        else:
+            return 32 - self.ones(abs(a) - 1)
+
+    def ones(self, n):
+        ret = 0
+        n &= 0xffffffff
+        while n != 0:
+            if n & 1:
+                ret += 1
+            n >>= 1
+        return ret
