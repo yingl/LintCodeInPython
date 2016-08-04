2d1f368e8aa57571b190801d8d1fade7b01388c4
yingl <linying_43151@163.com>
2016-08-04 09:12:21 +0800

    删除排序数组中的重复数字

---
+++ remove_duplicates_from_sorted_array.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param A: a list of integers
+    @return an integer
+    """
+    def removeDuplicates(self, A):
+        # write your code here
+        steps = 0
+        i = 0
+        while i < len(A):
+            number = A[i]
+            A[i - steps] = number
+            j = i + 1
+            while (j < len(A)) and (A[j] == number):
+                steps += 1
+                j += 1
+            i = j
+        return len(A) - steps
