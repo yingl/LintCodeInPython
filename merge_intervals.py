12940b9dbe320168d7404d16f8110a5c79b098b0
yingl <linying_43151@163.com>
2016-08-04 11:32:14 +0800

    合并区间

---
+++ merge_intervals.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param intervals, a list of Interval
+    # @return a list of Interval
+    def merge(self, intervals):
+        # write your code here
+        ret = []
+        intervals.sort(key = lambda x: x.start)
+        tmp = None
+        for interval in intervals:
+            if not tmp:
+                tmp = interval
+            else:
+                if (interval.start >= tmp.start) and (interval.start <= tmp.end):
+                    if interval.end > tmp.end:
+                        tmp.end = interval.end
+                else:
+                    ret.append(tmp)
+                    tmp = interval
+        if tmp:
+            ret.append(tmp)
+        return ret
