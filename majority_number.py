bba70188b34a411a319eac124c80092226382b54
yingl <linying_43151@163.com>
2016-08-04 13:37:00 +0800

    主元素

---
+++ majority_number.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param nums: A list of integers
+    @return: The majority number
+    """
+    def majorityNumber(self, nums):
+        # write your code here
+        # 借鉴majority_number_ii.py的算法，复杂度降低到O(n)。
+        ret, count = 0, 0
+        for i in xrange(len(nums)):
+            if count == 0:
+                ret = nums[i]
+                count = 1
+            else:
+                count = (count + 1) if ret == nums[i] else (count - 1)
+        return ret
