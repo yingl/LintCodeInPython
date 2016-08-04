40c3a803955740b3ef880dc2a6ff9a9ae41cdfd1
yingl <linying_43151@163.com>
2016-08-04 10:20:43 +0800

    链表倒数第n个节点

---
+++ nth_to_last_node_in_list.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param head: The first node of linked list.
+    @param n: An integer.
+    @return: Nth to last node of a singly linked list. 
+    """
+    def nthToLast(self, head, n):
+        # write your code here
+        before_node = head
+        after_node = head
+        for i in xrange(0, n):
+            before_node = before_node.next
+        while before_node is not None:
+            before_node = before_node.next
+            after_node = after_node.next
+        return after_node
