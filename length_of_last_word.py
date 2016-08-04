d686864239facb2260e17590ea079d21b73d0394
yingl <linying_43151@163.com>
2016-08-04 13:44:41 +0800

    最后一个单词的长度

---
+++ length_of_last_word.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param {string} s A string
+    # @return {int} the length of last word
+    def lengthOfLastWord(self, s):
+        # Write your code here
+        ret, i = 0, len(s) - 1
+        while i >= 0:
+            if s[i] != ' ':
+                ret += 1
+            elif ret > 0:
+                break
+            i -= 1
+        return ret
