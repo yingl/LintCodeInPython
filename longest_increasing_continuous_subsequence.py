bbb3aa0e02adf96155b726501068589ba7b52f3c
yingl <linying_43151@163.com>
2016-08-04 13:41:57 +0800

    最长上升连续子序列

---
+++ longest_increasing_continuous_subsequence.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param {int[]} A an array of Integer
+    # @return {int}  an integer
+    def longestIncreasingContinuousSubsequence(self, A):
+        # Write your code here
+        len_a = len(A)
+        if len_a <= 1:
+            return len_a
+        sign = 1 if A[0] < A[1] else -1
+        i, seq_count, max_seq_count = 1, 1, 0
+        while i < len_a:
+            if sign > 0:
+                if A[i] >= A[i - 1]:
+                    seq_count += 1
+                else:
+                    if seq_count > max_seq_count:
+                        max_seq_count = seq_count
+                    seq_count = 2
+                    sign = -1
+            else:
+                if A[i] <= A[i - 1]:
+                    seq_count += 1
+                else:
+                    if seq_count > max_seq_count:
+                        max_seq_count = seq_count
+                    seq_count = 2
+                    sign = 1
+            i += 1
+        return max_seq_count if max_seq_count >= seq_count else seq_count
