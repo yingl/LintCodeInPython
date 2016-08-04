bb25c4b73749ab0df3a5311a13f0978b02d5e481
yingl <linying_43151@163.com>
2016-08-04 13:51:48 +0800

    链表插入排序

---
+++ insertion_sort_list.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param head: The first node of linked list.
+    @return: The head of linked list.
+    """ 
+    def insertionSortList(self, head):
+        # write your code here
+        if not head:
+            return None
+        new_head = ListNode(0)
+        while head:
+            tmp = new_head
+            next = head.next
+            while tmp.next and tmp.next.val < head.val:
+                tmp = tmp.next
+            head.next = tmp.next
+            tmp.next = head
+            head = next
+        return new_head.next
