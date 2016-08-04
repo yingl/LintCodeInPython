92775965f51a526137b0879a8bb6c25ba0f74080
yingl <linying_43151@163.com>
2016-08-04 09:08:02 +0800

    子数组之和

---
+++ subarray_sum.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param nums: A list of integers
+    @return: A list of integers includes the index of the first number 
+             and the index of the last number
+    """
+    def subarraySum(self, nums):
+        # write your code here
+        for i in xrange(0, len(nums)):
+            sum = nums[i]
+            if sum == 0:
+                return [i, i]
+            for j in xrange(i + 1, len(nums)):
+                sum += nums[j]
+                if sum == 0:
+                    return [i, j]
