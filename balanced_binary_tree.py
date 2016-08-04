b1c1dbc678a048a643fcb6c56623d6214b3fb951
yingl <linying_43151@163.com>
2016-08-04 14:22:16 +0800

    平衡二叉树

---
+++ balanced_binary_tree.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: True if this Binary tree is Balanced, or false.
+    """
+    def isBalanced(self, root):
+        # write your code here
+        if not root:
+            return True
+        else:
+            if abs(self._isBalanced(root.left) - self._isBalanced(root.right)) <= 1:
+                return self.isBalanced(root.left) and self.isBalanced(root.right)
+            else:
+                return False
+
+    def _isBalanced(self, root):
+        if not root:
+            return 0
+        return 1 + max(self._isBalanced(root.left), self._isBalanced(root.right))
