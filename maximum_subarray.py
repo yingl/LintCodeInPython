41f50d663d78c022ae271913abd56a6a9e51fcf6
yingl <linying_43151@163.com>
2016-08-04 13:20:30 +0800

    最大子数组

---
+++ maximum_subarray.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param nums: A list of integers
+    @return: An integer denote the sum of maximum subarray
+    """
+    def maxSubArray(self, nums):
+        # write your code here
+        i, _sum, sum_max = 0, 0, -2147483648
+        while i < len(nums):
+            _sum += nums[i]
+            if _sum > sum_max:
+                sum_max = _sum
+            if _sum < 0:
+                _sum = 0
+            i += 1
+        return sum_max
