0ba756c5e5d2d2f7ed70fe794e2f940308ada230
yingl <linying_43151@163.com>
2016-08-04 11:14:22 +0800

    二叉树的最小深度

---
+++ minimum_depth_of_binary_tree.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: An integer
+    """ 
+    def minDepth(self, root):
+        # write your code here
+        self.min_depth = 2147483647
+        if not root:
+            return 0
+        self.dfs(root, 1)
+        return self.min_depth
+
+    def dfs(self, root, depth):
+        if not (root.left or root.right):
+            if depth < self.min_depth:
+                self.min_depth = depth
+        else:
+            if root.left:
+                self.dfs(root.left, depth + 1)
+            if root.right:
+                self.dfs(root.right, depth + 1)
