700f7ddaff0da45fd44da370f76484f333999b3b
yingl <linying_43151@163.com>
2016-08-04 13:55:01 +0800

    在二叉查找树中插入节点

---
+++ insert_node_in_a_binary_search_tree.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param root: The root of the binary search tree.
+    @param node: insert this node into the binary search tree.
+    @return: The root of the new binary search tree.
+    """
+    def insertNode(self, root, node):
+        # write your code here
+        if root is None:
+            return node
+        if root.val > node.val:
+            root.left = self.insertNode(root.left, node)
+        else:
+            root.right = self.insertNode(root.right, node)
+        return root
