29da7c8832758c47f47ad122be0b2b19f5ec26b8
yingl <linying_43151@163.com>
2016-08-04 09:26:49 +0800

    加一

---
+++ plus_one.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param {int[]} digits a number represented as an array of digits
+    # @return {int[]} the result
+    def plusOne(self, digits):
+        # Write your code here
+        number = 0
+        for i in xrange(0, len(digits)):
+            number = number * 10 + digits[i]
+        number += 1
+        ret = []
+        while number > 0:
+            ret.append(number % 10)
+            number /= 10
+        ret.reverse()
+        return ret
