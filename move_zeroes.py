f29d20ff801e493e7c508e53926636531bbc7f77
yingl <linying_43151@163.com>
2016-08-04 10:23:47 +0800

    移动零

---
+++ move_zeroes.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param {int[]} nums an integer array
+    # @return nothing, do this in-place
+    def moveZeroes(self, nums):
+        # Write your code here
+        zeros = 0
+        for i in xrange(0, len(nums)):
+            nums[i - zeros] = nums[i]
+            if nums[i] == 0:
+                zeros += 1
+        for i in xrange(zeros):
+            nums[len(nums) - i - 1] = 0
