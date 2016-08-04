f64884aeb88ece57f6f96d16fc944eca9f6ebce8
yingl <linying_43151@163.com>
2016-08-04 14:10:56 +0800

    在O(1)时间复杂度删除链表节点

---
+++ delete_node_in_the_middle_of_singly_linked_list.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param node: the node in the list should be deleted
+    # @return: nothing
+    def deleteNode(self, node):
+        # write your code here
+        if node.next is None:
+            node = None
+        else:
+            p = node.next
+            node.val = p.val
+            node.next = p.next
