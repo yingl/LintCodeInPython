7aa0f81ab696c899a6f2534fa3f2fcc37c03ed21
yingl <linying_43151@163.com>
2016-08-04 13:57:42 +0800

    等价二叉树

---
+++ identical_binary_tree.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param a, b, the root of binary trees.
+    @return true if they are identical, or false.
+    """
+    def isIdentical(self, a, b):
+        # Write your code here
+        if not (a or b):
+            return True
+        if a and b:
+            if a.val == b.val:
+                return self.isIdentical(a.left, b.left) and self.isIdentical(a.right, b.right)
+        return False
