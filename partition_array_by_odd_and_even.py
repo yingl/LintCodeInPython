7e8b8e58c4987a3e6477e8c6a1a5a38540fc5bea
yingl <linying_43151@163.com>
2016-08-04 09:34:16 +0800

    奇偶分割数组

---
+++ partition_array_by_odd_and_even.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param nums: a list of integers
+    # @return: nothing
+    def partitionArray(self, nums):
+        # write your code here
+        i = 0
+        j = len(nums) - 1
+        while i < j:
+            if (nums[i] % 2 == 0) and (nums[j] % 2 == 1):
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
+            else:
+                if (nums[i] % 2 == 1):
+                    i += 1
+                if (nums[j] % 2 == 0):
+                    j -= 1
