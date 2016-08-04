98c144fa2e1f9cc0cf061867f74cbddb5925d20d
yingl <linying_43151@163.com>
2016-08-04 14:03:56 +0800

    快乐数

---
+++ happy_number.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param {int} n an integer
+    # @return {boolean} true if this is a happy number or false
+    def isHappy(self, n):
+        # Write your code here
+        ret = {}
+        while True:
+            digits = []
+            while n > 0:
+                digits.append(n % 10)
+                n /= 10
+            n = 0
+            for d in digits:
+                n += d * d
+            if n == 1:
+                return True
+            if n in ret:
+                return False
+            else:
+                ret[n] = True
