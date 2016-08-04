ab8fa346109df84445f077f24e0ee3a9c5d1c573
yingl <linying_43151@163.com>
2016-08-04 14:14:12 +0800

    二进制中有多少个1

---
+++ count_1_in_binary.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param num: an integer
+    # @return: an integer, the number of ones in num
+    def countOnes(self, num):
+        # write your code here
+        ret = 0
+        num &= 0xffffffff
+        while num != 0:
+            if num & 1:
+                ret += 1
+            num >>= 1
+        return ret
