b74666cdb8ac64b64c18df699e75f5a7317295b8
yingl <linying_43151@163.com>
2016-08-04 13:21:43 +0800

    二叉树的最大深度

---
+++ maximum_depth_of_binary_tree.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: An integer
+    """ 
+    def maxDepth(self, root):
+        # write your code here
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
