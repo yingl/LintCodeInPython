79ee689dae5eeaa741719ad7b0d600617cf4a61d
yingl <linying_43151@163.com>
2016-08-09 15:46:17 +0800

    最长公共前缀

---
+++ longest_common_prefix.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param strs: A list of strings
+    # @return: The longest common prefix
+    def longestCommonPrefix(self, strs):
+        # write your code here
+        ret = []
+        i = 0
+        while True:
+            ch = None
+            for _str in strs:
+                if i >= len(_str):
+                    ch = None
+                    break
+                if not ch:
+                    ch = _str[i]
+                else:
+                    if ch != _str[i]:
+                        ch = None
+                        break
+            if ch:
+                ret.append(ch)
+                i += 1
+            else:
+                break
+        return ''.join(ret)
