0e9974804874701bdb15c3def428da6a77da1c1e
yingl <linying_43151@163.com>
2016-08-10 23:51:31 +0800

    买卖股票的最佳时机 II

---
+++ best_time_to_buy_and_sell_stock_ii.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param prices: Given an integer array
+    @return: Maximum profit
+    """
+    def maxProfit(self, prices):
+        # write your code here
+        ret = 0
+        buy_price = -1
+        for i in xrange(len(prices) - 1): # 最后一天只能卖
+            if buy_price == -1: # 寻找买点
+                if prices[i] < prices[i + 1]:
+                    buy_price = prices[i]
+                else:
+                    continue
+            else: # 寻找卖点，这里sell_price肯定大于buy_price。
+                if prices[i] > prices[i + 1]:
+                    ret += prices[i] - buy_price
+                    buy_price = -1
+        if buy_price != -1:
+            ret += prices[-1] - buy_price
+        return ret
