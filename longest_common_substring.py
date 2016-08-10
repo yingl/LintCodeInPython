86a35ffb9d53551e55957af82c10128db12de150
yingl <linying_43151@163.com>
2016-08-10 15:49:44 +0800

    最长公共子串

---
+++ longest_common_substring.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param A, B: Two string.
+    # @return: the length of the longest common substring.
+    def longestCommonSubstring(self, A, B):
+        # write your code here
+        # 如果A[i]等于B[j]，那么子串的长度等于c[i - 1][j - 1] ＋ 1。
+        ret = 0
+        cache = [[0] * (len(B) + 1) for i in xrange(len(A) + 1)]
+        for i in xrange(1, len(A) + 1):
+            for j in xrange(1, len(B) + 1):
+                cache[i][j] = cache[i - 1][j - 1] + 1 if A[i - 1] == B[j - 1] else 0
+                ret = max(cache[i][j], ret)
+        return ret
