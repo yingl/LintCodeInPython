881809c0af6b2790204d9f2c3f5486e6e7f35fb8
yingl <linying_43151@163.com>
2016-08-10 18:06:14 +0800

    两个链表的交叉

---
+++ intersection_of_two_linked_lists.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param headA: the first list
+    # @param headB: the second list
+    # @return: a ListNode
+    def getIntersectionNode(self, headA, headB):
+        # Write your code here
+        list_a_len = self.listLen(headA)
+        list_b_len = self.listLen(headB)
+        steps = abs(list_a_len - list_b_len)
+        for i in xrange(steps):
+            if list_a_len > list_b_len:
+                headA = headA.next
+            else:
+                headB = headB.next
+            i += 1
+        while True:
+            if headA != headB:
+                headA = headA.next
+                headB = headB.next
+            else:
+                return headA
+
+    def listLen(self, head):
+        list_len = 0
+        while head:
+            list_len += 1
+            head = head.next
+        return list_len
