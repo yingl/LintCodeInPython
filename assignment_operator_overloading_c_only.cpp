ebffbf01de3c9df9dd6bbdcf482c4b78e42853b6
yingl <linying_43151@163.com>
2016-08-10 23:32:50 +0800

    赋值运算符重载

---
+++ assignment_operator_overloading_c_only.cpp
@@ -0,0 +1,29 @@
+// 此题仅针对C++有效
+
+class Solution {
+public:
+    char *m_pData;
+    Solution() {
+        this->m_pData = NULL;
+    }
+    Solution(char *pData) {
+        this->m_pData = pData;
+    }
+
+    // Implement an assignment operator
+    Solution operator=(const Solution &object) {
+        // write your code here
+        if (this != &object) {
+            delete[] m_pData;
+            if (object.m_pData != NULL) {
+                int len = strlen(object.m_pData);
+                m_pData = new char[len + 1];
+                strcpy(m_pData, object.m_pData);
+            }
+            else {
+                m_pData = NULL;
+            }
+        }
+        return *this;
+    }
+};
