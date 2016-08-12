fd60fdd4643581f1667df76c86f3bae25d4a69d7
yingl <linying_43151@163.com>
2016-08-12 10:39:41 +0800

    x的n次幂

---
+++ powx_n.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param {double} x the base number
+    # @param {int} n the power number
+    # @return {double} the result
+    def myPow(self, x, n):
+        # Write your code here
+        if n == 0:
+            return 1
+        elif n == 1:
+            return x
+        elif n < 0:
+            return 1 / self.myPow(x, -n)
+        else:
+            k = self.myPow(x, n / 2)
+            k2 = k * k
+            return k2 if (n % 2 == 0) else (k2 * x)
