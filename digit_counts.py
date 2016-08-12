61840b8cbbba678aaac1d7791951d20d72c8f4c8
yingl <linying_43151@163.com>
2016-08-12 11:05:38 +0800

    统计数字

---
+++ digit_counts.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param k & n  two integer
+    # @return ans a integer
+    def digitCounts(self, k, n):
+        ret = 0
+        for i in xrange(n + 1):
+            ret += self.check_k(i, k)
+        return ret
+        
+    def check_k(self, i, k):
+        if i == k:
+            return 1
+        else:
+            ret = 0
+            while i != 0:
+                if (i % 10) == k:
+                    ret += 1
+                i /= 10
+            return ret
