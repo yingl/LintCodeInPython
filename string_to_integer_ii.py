6e8f4c70c61f1e7936b324329aad104d96d26b0e
yingl <linying_43151@163.com>
2016-08-12 11:14:22 +0800

    转换字符串到整数

---
+++ string_to_integer_ii.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    # @param str: a string
+    # @return an integer
+    def atoi(self, str):
+        # write your code here
+        int_max = 2147483647
+        int_min = -2147483648
+        sum = 0
+        i = 0
+        str = str.strip()
+        if not str:
+            return 0
+        sign = 1
+        i = 0
+        if str[0] == '-':
+            sign = -1
+            i = 1
+        elif str[0] == '+':
+            i = 1
+        while i < len(str):
+            if not str[i].isdigit():
+                break
+            digit = int(str[i])
+            if int_max / 10 > sum:
+                sum *= 10
+            else:
+                return int_max if sign > 0 else int_min
+            if int_max - digit >= sum:
+                sum += digit
+            else:
+                return int_max if sign > 0 else int_min
+            i += 1
+        return sign * sum
