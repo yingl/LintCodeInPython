177bc320590df2301fc66206d8adac5b2ffb0ca2
yingl <linying_43151@163.com>
2016-08-12 10:47:24 +0800

    带最小值操作的栈

---
+++ min_stack.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+class MinStack(object):
+    def __init__(self):
+        # do some intialize if necessary
+        self.stack = []
+        self.mins = []
+
+    def push(self, number):
+        # write yout code here
+        self.stack.append(number)
+        if self.mins and (number > self.mins[-1]):
+            self.mins.append(self.mins[-1])
+        else:
+            self.mins.append(number)
+
+    def pop(self):
+        # pop and return the top item in stack
+        if self.stack:
+            self.mins.pop()
+            return self.stack.pop()
+
+    def min(self):
+        # return the minimum number in stack
+        if self.mins:
+            return self.mins[-1]
