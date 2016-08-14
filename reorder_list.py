36597160427bf63c020d833c1f250f83463ac079
yingl <linying_43151@163.com>
2016-08-14 14:24:35 +0800

    内容错误，重新提交

--- reorder_list.py
+++ reorder_list.py
@@ -7,17 +7,43 @@ class Solution:
     """
     def reorderList(self, head):
         # write your code here
+    def reorderList(self, head):
+        # write your code here
         '''
         0->1->2->3->4->5 (n = 5) => 0->5->1->4->2->3 (012/543)
         0->1->2->3->4->5->6 (n = 6) => 0->6->1->5->2->4->3 (0123/654)
         '''
         if head:
-            slow, fast = head, head # fast每次前进2步
+            prev, slow, fast = None, head, head # fast每次前进2步
             while fast:
+                prev = slow
+                slow = slow.next
                 fast = fast.next
                 if not fast:
                     break
                 fast = fast.next
-                slow = slow.next
-                print slow.val, fast.val
-            
+            old_tail = prev
+            old_tail.next = None
+            new_head = slow
+            new_head = self.reverse(new_head)
+            while head:
+                if new_head:
+                    _next = head.next
+                    head.next = new_head
+                    new_head = new_head.next
+                    head.next.next = _next
+                    head = _next
+                else:
+                    head = head.next
+
+    def reverse(self, head):
+        # write your code here
+        if not head:
+            return None
+        new_head = None
+        while head:
+            node = head
+            head = head.next
+            node.next = new_head
+            new_head = node
+        return new_head
