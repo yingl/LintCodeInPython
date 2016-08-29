4530cc3ff7fa6319ac23898e3204f5e35d30c9f8
yingl <linying_43151@163.com>
2016-08-29 18:36:35 +0800

    克隆二叉树

---
+++ clone_binary_tree.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    """
+    @param {TreeNode} root: The root of binary tree
+    @return {TreeNode} root of new tree
+    """
+    def cloneTree(self, root):
+        # Write your code here
+        new_root = None
+        if root:
+            new_root = TreeNode(root.val)
+            if root.left:
+                new_root.left = self.cloneTree(root.left)
+            if root.right:
+                new_root.right = self.cloneTree(root.right)
+        return new_root
