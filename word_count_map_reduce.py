122b2b887335a82398bb5f5948db9e4c848d7790
yingl <linying_43151@163.com>
2016-09-23 14:38:43 +0800

    单词计数 (Map Reduce版本)

---
+++ word_count_map_reduce.py
@@ -0,0 +1,17 @@
+class WordCount:
+
+    # @param {str} line a text, for example "Bye Bye see you next"
+    def mapper(self, _, line):
+        # Write your code here
+        # Please use 'yield key, value'
+        words = line.split()
+        for word in words:
+            yield word, 1
+
+
+    # @param key is from mapper
+    # @param values is a set of value with the same key
+    def reducer(self, key, values):
+        # Write your code here
+        # Please use 'yield key, value'
+        yield key, len(values)
