c4f1c9155e877af3357030d5910da3e44f127f6a
yingl <linying_43151@163.com>
2016-09-23 14:08:21 +0800

    将数组重新排序以构造最小值

---
+++ reorder_array_to_construct_the_minimum_number.py
@@ -0,0 +1,8 @@
+class Solution:
+    # @param {int[]} nums n non-negative integer array
+    # @return {str} a string
+    def minNumber(self, nums):
+        # Write your code here
+        # 修改一下排序规则就可以了
+        nums.sort(lambda x, y: cmp(str(x) + str(y), str(y) + str(x)))
+        return str(int(''.join(str(i) for i in nums)))  # 把前面的0都去掉
