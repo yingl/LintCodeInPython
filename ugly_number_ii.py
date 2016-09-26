a11f1df8f1f1681eae8887ab99359daaeca2cf53
yingl <linying_43151@163.com>
2016-09-26 23:29:13 +0800

    增加算法解释

--- ugly_number_ii.py
+++ ugly_number_ii.py
@@ -7,6 +7,48 @@ class Solution:
     """
     def nthUglyNumber(self, n):
         # write your code here
+        '''
+        实现分析：
+        i = 1
+            m_2 = 2
+            m_3 = 3
+            m_5 = 5
+            numbers[1] = 2
+            i_2 += 1 => 1
+            i_3 = 0
+            i_5 = 0
+        i = 2
+            m_2 = numbers[1] * 2 = 2 * 2 = 4
+            m_3 = 3
+            m_5 = 5
+            numbers[2] = 3
+            i_2 ＝ 1
+            i_3 += 1 => 1
+            i_5 = 0
+        i = 3
+            m_2 = numbers[1] * 2 ＝ 4
+            m_3 = numbers[1] * 3 = 6
+            m_5 = 5
+            numbers[3] = 4
+            i_2 += 1 => 2
+            i_3 = 1
+            i_5 = 0
+        i = 4
+            m_2 = numbers[2] * 2 = 6
+            m_3 = numbers[1] * 3 = 6
+            m_5 = 5
+            numbers[4] = 5
+            i_2 = 2
+            i_3 = 1
+            i_5 = 1
+        i = 5
+            m_2 = numbers[2] * 2 = 6
+            m_3 = numbers[1] * 3 = 6
+            m_5 = numbers[1] * 5 = 10
+            numbers[5] = 6
+        ......
+
+        '''
         numbers = [1] * n # 初始值
         index_2 = 0 # 对应乘235的最小值索引
         index_3 = 0
