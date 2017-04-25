f582d27b57811f466b6ca5b72617652e14d87d76
yingl <linying_43151@163.com>
2017-04-25 17:40:19 +0800

    Update reorder_array_to_construct_the_minimum_number.py

--- reorder_array_to_construct_the_minimum_number.py
+++ reorder_array_to_construct_the_minimum_number.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 class Solution:
     # @param {int[]} nums n non-negative integer array
     # @return {str} a string
@@ -6,3 +8,5 @@ class Solution:
         # 修改一下排序规则就可以了
         nums.sort(lambda x, y: cmp(str(x) + str(y), str(y) + str(x)))
         return str(int(''.join(str(i) for i in nums)))  # 把前面的0都去掉
+
+# medium: http://lintcode.com/zh-cn/problem/reorder-array-to-construct-the-minimum-number/
