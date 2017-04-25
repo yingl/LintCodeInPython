5fe22cd0e537d1b1b2e757addce0ac2f372c34c2
yingl <linying_43151@163.com>
2017-04-25 13:07:49 +0800

    Update minimum_depth_of_binary_tree.py

--- minimum_depth_of_binary_tree.py
+++ minimum_depth_of_binary_tree.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 
 class Solution:
     """
@@ -22,3 +22,5 @@ class Solution:
                 self.dfs(root.left, depth + 1)
             if root.right:
                 self.dfs(root.right, depth + 1)
+
+# easy: http://lintcode.com/zh-cn/problem/minimum-depth-of-binary-tree/
