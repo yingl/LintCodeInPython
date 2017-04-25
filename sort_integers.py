8d7e3c3d379609e34e52678c69095af8502ed999
yingl <linying_43151@163.com>
2017-04-25 12:12:33 +0800

    Update sort_integers.py

--- sort_integers.py
+++ sort_integers.py
@@ -1,11 +1,13 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 
 class Solution:
     # @param {int[]} A an integer array
     # @return nothing
     def sortIntegers(self, A):
         # Write your code here
-        for i in xrange(0, len(A)):
-            for j in xrange(i, len(A)):
+        for i in range(len(A)):
+            for j in range(i, len(A)):
                 if A[i] > A[j]:
                     A[i], A[j] = A[j], A[i]
+
+# entry: http://www.lintcode.com/problem/sort-integers
