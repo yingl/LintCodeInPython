f324e472bd21008f0fac5de8d59f8e53815cfffa
yingl <linying_43151@163.com>
2017-04-25 17:05:27 +0800

    Update wiggle_sort.py

--- wiggle_sort.py
+++ wiggle_sort.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 
 class Solution(object):
     """
@@ -18,3 +18,5 @@ class Solution(object):
             if ((i % 2 == 1) and (nums[i] < nums[i - 1])) or \
                 ((i % 2 == 0) and (nums[i] > nums[i - 1])):
                 nums[i], nums[i - 1] = nums[i- 1], nums[i]
+
+# medium: http://lintcode.com/zh-cn/problem/wiggle-sort/
