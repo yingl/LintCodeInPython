fe0f354c51e61bff5dfd1b141ec6354e58aa3399
yingl <linying_43151@163.com>
2017-04-25 17:21:33 +0800

    Update sort_colors_ii.py

--- sort_colors_ii.py
+++ sort_colors_ii.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 
 class Solution:
     """
@@ -12,3 +12,5 @@ class Solution:
         # 已知条件包括：包括k种不同的颜色，并按照1到k进行编号。
         # 所以pivot的选取就比较简单了，从1开始往上加。
         colors.sort()
+
+# medium: http://lintcode.com/zh-cn/problem/sort-colors-ii/
