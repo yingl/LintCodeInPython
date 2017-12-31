68def8dac3da06cc7d22d5c8756f979263922858
yingl <linying_43151@163.com>
2017-12-31 22:05:40 +0800

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+class Solution:
+    """
+    @param: : an integer
+    @return:  the factorial of n
+    """
+
+    def factorial(self, n):
+        # write your code here
+        # 偷懒做法，实际就是做竖式乘法，Python内部实现了。
+        f = 1
+        for i in range(2, n + 1):
+            f *= i
+        return str(f)
+        
+# medium: http://lintcode.com/zh-cn/problem/factorial/
