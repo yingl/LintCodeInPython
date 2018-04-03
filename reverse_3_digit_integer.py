c42fbe30f2999035dbe9533bcfe517192112ba71
yingl <linying_43151@163.com>
2018-04-03 10:24:11 +0800

    Create reverse_3_digit_integer.py

---
+++ reverse_3_digit_integer.py
@@ -0,0 +1,14 @@
+class Solution:
+    """
+    @param number: A 3-digit number.
+    @return: Reversed number.
+    """
+    def reverseInteger(self, number):
+        # write your code here
+        ret = 0
+        while number != 0:
+            ret = ret * 10 + (number % 10)
+            number /= 10
+        return ret
+
+# Easy: http://www.lintcode.com/zh-cn/problem/reverse-3-digit-integer/
