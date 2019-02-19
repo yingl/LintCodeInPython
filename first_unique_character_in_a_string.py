6c51f2db10d24722f6969cc9c66209381e1262af
yingl <linying_43151@163.com>
2019-02-19 21:39:02 +0800

    Create first_unique_character_in_a_string.py

---
+++ first_unique_character_in_a_string.py
@@ -0,0 +1,18 @@
+class Solution:
+    """
+    @param str: str: the given string
+    @return: char: the first unique character in a given string
+    """
+    def firstUniqChar(self, str):
+        # Write your code here
+        # a ~ z = 65 ~ 90
+        # A ~ Z = 97 ~ 122
+        # 122 - 65 + 1 = 58
+        data = [0] * 58 # 计数统计
+        for c in str:
+            data[ord(c) - 65] += 1
+        for c in str: # 看首先出现次数为1的就返回
+            if data[ord(c) - 65] == 1:
+                return c
+                
+# easy: https://www.lintcode.com/problem/first-unique-character-in-a-string/description
