c672b38aa266433576d4c4b2d15956442ce71080
yingl <linying_43151@163.com>
2019-02-19 22:18:28 +0800

    Update counting_bits.py

--- counting_bits.py
+++ counting_bits.py
@@ -6,15 +6,16 @@ class Solution:
     def countBits(self, num):
         # write your code here
         # 这实际是个数学题。
-        # 因为010和101的1的数量是一样的。但如果i是偶数的话，下一个数的1的数量就加1。
+        # 1. 如果是偶数，和除2后的数的1一样多。
+        # 2. 如果是奇数，就是前一个偶数的1的数量再加1。
         if num <= 0:
             return [0]
         ret = [0] * (num + 1)
-        for i in range(1, num+1):
-            if (i % 2) == 0:
+        for i in range(1, num + 1):
+            if (i % 2) != 0:
                 ret[i] = ret[i - 1] + 1
             else:
-                ret[i] = ret[i / 2]
-        retutn ret
+                ret[i] = ret[int(i / 2)]
+        return ret
 
 # medium: https://www.lintcode.com/problem/counting-bits
