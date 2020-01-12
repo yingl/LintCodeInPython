c23c162684224d434a9c766f71ce83f8acedc68f
yingl <linying_43151@163.com>
2020-01-12 12:19:21 +0800

    Create to-lower-case.py

---
+++ to-lower-case.py
@@ -0,0 +1,10 @@
+class Solution:
+    """
+    @param str: the input string
+    @return: The lower case string
+    """
+    def toLowerCase(self, str):
+        # Write your code here
+        return str.lower() # 懒得具体写了...
+        
+# easy: https://www.lintcode.com/problem/to-lower-case/
