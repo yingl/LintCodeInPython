2cd2a931ecc01aada73c2804e45204b125777dbc
yingl <linying_43151@163.com>
2020-01-16 20:51:22 +0800

    Create rotate-array.py

---
+++ rotate-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    """
+    @param nums: an array
+    @param k: an integer
+    @return: rotate the array to the right by k steps
+    """
+    def rotate(self, nums, k):
+        # Write your code here
+        k = k % len(nums)
+        self.reverse(nums, 0, len(nums) - k - 1)
+        self.reverse(nums, len(nums) - k, len(nums) - 1)
+        self.reverse(nums, 0, len(nums) - 1)
+        return nums
+        
+    def reverse(self, nums, start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+
+# easy: https://www.lintcode.com/problem/rotate-array/
