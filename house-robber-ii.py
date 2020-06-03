d8a7c2f2593c9e1dd86c5b4828a0f7522ebae670
yingl <linying_43151@163.com>
2020-06-03 16:47:39 +0800

    Update house-robber-ii.py

--- house-robber-ii.py
+++ house-robber-ii.py
@@ -1,34 +1,31 @@
-# coding: utf-8
-
 class Solution:
-    # @param nums: A list of non-negative integers.
-    # return: an integer
+    """
+    @param nums: An array of non-negative integers.
+    @return: The maximum amount of money you can rob tonight
+    """
     def houseRobber2(self, nums):
         # write your code here
-        '''
-        与打劫房屋相比：
-        1. 如果我抢了第1间，最后一间就不能抢。
-        2. 如果第一间不抢，就可以抢最后一间。
-        '''
-        if not nums:
+        # 第一间与最后一间不能同时抢
+        if (not nums) or (len(nums) == 2):
             return 0
-        elif len(nums) == 1:
+        if len(nums) == 1:
             return nums[0]
-        elif len(nums) == 2:
-            return max(nums[0], nums[1])
-        else:
-            ret_1, ret_2 = 0, 0
-            # 场景1
-            caches = nums[:]
-            caches[1] = max(caches[0], caches[1])
-            for i in xrange(2, len(nums) - 1):
-                caches[i] = max(caches[i] + caches[i - 2], caches[i - 1])
-            ret_1 = caches[-2]
-            caches = nums[:]
-            caches[0] = 0
-            for i in xrange(2, len(nums)):
-                caches[i] = max(caches[i] + caches[i - 2], caches[i - 1])
-            ret_2 = caches[-1]
-            return max(ret_1, ret_2)
+        dp_1 = [0] * (len(nums) - 1) # 0到倒数第2间
+        dp_2 = [0] * (len(nums) - 1) # 1到最后一间
+        for i in range(0, len(nums) - 1):
+            if i == 0:
+                dp_1[i] = nums[0]
+            if i == 1:
+                dp_1[i] = max(nums[0], nums[1])
+            if i > 1:
+                dp_1[i] = max(dp_1[i - 2] + nums[i], dp_1[i - 1])
+        for i in range(1, len(nums)):
+            if i == 1:
+                dp_2[i - 1] = nums[1]
+            if i == 2:
+                dp_2[i - 1] = max(nums[1], nums[2])
+            if i > 2:
+                dp_2[i - 1] = max(dp_2[i - 3] + nums[i], dp_2[i - 2])
+        return max(dp_1[-1], dp_2[-1])
 
 # medium: http://lintcode.com/zh-cn/problem/house-robber-ii/
