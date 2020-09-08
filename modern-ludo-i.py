48f17fd3ff6377f4ef58412f5e17b0f082b651c6
yingl <linying_43151@163.com>
2020-09-09 00:29:08 +0800

    Update modern-ludo-i.py

--- modern-ludo-i.py
+++ modern-ludo-i.py
@@ -7,7 +7,6 @@ class Solution:
     def modernLudo(self, length, connections):
         # Write your code here
         # 7步的情况：1的话走1步到2，走6步到7，丢1次足够了...
-        ret = 1
         if length <= 7:
             return 1
         sc = [0] # 短路表，因为从1开始，所以都额外加个元素。
@@ -15,16 +14,13 @@ class Solution:
         for i in range(1, length + 1):
             sc.append(i) # 自己和自己一定短路
             dp.append(length + 1)
-        dp[1] = 0 # 一开始就在位置0
+        dp[1] = 0 # 一开始就在位置1
         for i in range(len(connections)):
             sc[connections[i][0]] = connections[i][1] # 短路表内容
         for i in range(2, length + 1):
-            x = ''
             if (i - 6) <= 1:
-                x = 1
-                dp[i] = 1 # 丢一次肯定能到
+                dp[i] = 1 # 丢1次肯定能到
             else:
-                x = 2
                 for j in range(i - 1, i - 7, -1): # 往前看6步
                     dp[i] = min(dp[j] + 1, dp[i]) # j丢1次肯定能到i
             dp[sc[i]] = min(dp[i], dp[sc[i]]) # 更新第i个点对应的短路位置
