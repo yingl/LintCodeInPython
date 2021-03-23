a90e3cdc93084d21b6cb54bb61d3067185d63649
yingl <linying_43151@163.com>
2021-03-23 11:23:29 +0800

    Update subarray-sum-closest.py
    原有测试案例不严谨，修复代码bug。

--- subarray-sum-closest.py
+++ subarray-sum-closest.py
@@ -1,30 +1,25 @@
-# coding: utf-8
-
 class Solution:
     """
-    @param nums: A list of integers
-    @return: A list of integers includes the index of the first number 
-             and the index of the last number
+    @param: nums: A list of integers
+    @return: A list of integers includes the index of the first number and the index of the last number
     """
     def subarraySumClosest(self, nums):
         # write your code here
-        # 序列a[i + 1]到a[j]的和等于a[0]到a[j]的和减去a[0]到a[i]的和。
         if not nums:
             return []
         sums = []
         tmp = 0
-        for i in xrange(len(nums)):
+        for i in range(len(nums)):
             tmp += nums[i]
             sums.append([tmp, i])
         sums.sort()
-        ret = [0, 0]
-        diff = 2147483647
-        for i in xrange(1, len(sums)):
+        diff = abs(sums[0][0])
+        ret = [0, sums[0][1]]
+        for i in range(1, len(sums)):
             if abs(sums[i][0] - sums[i - 1][0]) < diff:
                 diff = abs(sums[i][0] - sums[i - 1][0])
-                ret = []
-                ret.append(min(sums[i][1], sums[i - 1][1]) + 1)
-                ret.append(max(sums[i][1], sums[i - 1][1]))
+                ret[0] = min(sums[i][1], sums[i - 1][1]) + 1
+                ret[1] = max(sums[i][1], sums[i - 1][1])
         return ret
 
 # medium: http://lintcode.com/problem/subarray-sum-closest
