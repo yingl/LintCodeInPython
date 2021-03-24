b8e712921aa62f5d79595dd5e01fff87406db4b4
yingl <linying_43151@163.com>
2021-03-24 11:16:24 +0800

    Update permutation-index.py
    缓存阶乘结果

--- permutation-index.py
+++ permutation-index.py
@@ -1,18 +1,20 @@
-# coding: utf-8
-
 class Solution:
-    # @param {int[]} A an integer array
-    # @return {long} a long integer
+    """
+    @param A: An array of integers
+    @return: A long integer
+    """
     def permutationIndex(self, A):
-        # Write your code here
+        # write your code here
         ret = 1
-        for i in xrange(0, len(A)):
-            fact, rc = 1, 0  # 初始化阶乘与逆序数
-            for j in xrange(i + 1, len(A)):
+        facts = [1]
+        for i in range(1, len(A)):
+            facts.append(facts[-1] * i)
+        for i in range(len(A)):
+            rc = 0 # 逆序数
+            for j in range(i + 1, len(A)):
                 if A[i] > A[j]:
                     rc += 1
-                fact *= (j - i)
-            ret += rc * fact
+            ret += rc * facts[len(A) - i - 1]
         return ret
 
 # easy: http://lintcode.com/zh-cn/problem/permutation-index/
